df82b6e25eee455759efba10f4aababe16513d16
lolla-sai <80596780+lolla-sai@users.noreply.github.com>
2022-06-27 01:44:55 +0530

    (Temp) disabled eslint

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    eslint: { ignoreDuringBuilds: true },
     images: {
         domains: [
             "lh3.googleusercontent.com",
@@ -11,3 +12,6 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+
+
+// ignore ES Lint is not a good practice. It is temporary for now
